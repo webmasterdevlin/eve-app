62e6fbd1ec5185caf5b466cadc1a8da67758cc8d
webmasterdevlin <webmasterdevlin@gmail.com>
2018-05-27 15:23:25 +0800

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { st } from "@angular/core/src/render3";
+import { LocationService } from "./location.service";
+import { City } from "./City";
+import { Observable } from "rxjs";
+import {MatSnackBar} from '@angular/material';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  lng: number = 121.0218146;
+  lat: number = 14.5537947;
+  newLng: number;
+  newLat: number;
+  loc: City;
+
+  constructor(private locService: LocationService, public snackBar: MatSnackBar) {}
+  onChoseLocation(event) {
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+  }
+  getMap() {
+    this.locService.getCity("3");
+    this.newLng = 121.0230918;
+    this.newLat = 14.5534097;
+    this.openSnackBar();
+  }
+
+  // TODO: Refactor this hardcoded function
+  find(city: string) {
+    this.locService.getCity("3").subscribe(data =>
+      // {
+      //   this.loc = data;
+      //   this.newLat = this.loc.lat;
+      //   this.newLng = this.loc.lng;
+      //   console.log(this.loc.lat + " "+ this.loc.lng);
+      //   this.getMap();
+      // }
+      this.getMap()
+    );
+  }
+
+  openSnackBar() {
+    this.snackBar.open("Searching for women drivers...", "OK", {
+      duration: 5000
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AgmCoreModule} from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeNavComponent } from './home-nav/home-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {LocationService} from './location.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MaterialModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey:''
+      // apiKey:" ",
+    }),
   ],
-  providers: [],
+  providers: [LocationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
